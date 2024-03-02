5f7d94aa70682919c35b9d546f1a3350eaaf85c9
Thiago Henrique <thiagohp070@gmail.com>
2024-03-02 16:41:16 -0300

    ref: remove generics

--- src/main/java/br/edu/ufersa/ClientApplicationMain.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ufersa;
-
-import br.edu.ufersa.client.Client;
-
-public class ClientApplicationMain {
-    public static void main(String[] args) {
-        new Client("127.0.0.1", 8080);
-    }
-}
